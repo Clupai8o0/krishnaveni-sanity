82fdf25bcb7e2183bb977d731dbe4dd5eca250cc
Clupai8o0 <clupaio4@gmail.com>
2025-06-09 20:37:42 +1000

    base pages defined and running

---
+++ client/app/[lang]/campus-life/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function CampusLifePage() {
+  return (
+    <div>CampusLifePage</div>
+  )
+}
+
+export default CampusLifePage
---
+++ client/app/[lang]/parents/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function ParentsPage() {
+  return (
+    <div>ParentsPage</div>
+  )
+}
+
+export default ParentsPage
