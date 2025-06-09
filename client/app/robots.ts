a97ed6fb7d215b97927922b764c6ceec3c2537a5
Clupai8o0 <clupaio4@gmail.com>
2025-06-09 15:57:47 +1000

    robots txt works with localization test routes disallowed

--- client/app/robots.ts
+++ client/app/robots.ts
@@ -1,11 +1,13 @@
 import { MetadataRoute } from 'next'
 
+import { SUPPORTED_LANGUAGES } from '@/lib/constants'
+
 export default function robots(): MetadataRoute.Robots {
   return {
     rules: {
       userAgent: '*',
       allow: '/',
-      disallow: '/test/',
+      disallow: SUPPORTED_LANGUAGES.map((lang) => `/${lang}/test/`),
     },
   }
 } 
