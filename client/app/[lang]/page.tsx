8490b59176b41bee9356fc6a7562fa78cccf24c0
Clupai8o0 <clupaio4@gmail.com>
2025-06-11 17:37:47 +1000

    hero section

--- client/app/[lang]/page.tsx
+++ client/app/[lang]/page.tsx
@@ -1,11 +1,35 @@
+import { client } from "@/lib/sanity";
+import { SanityDocument } from "next-sanity";
+
 import Navbar from "@/components/navbar";
 import Hero from "@/components/hero";
 
-function Homepage() {
+async function Homepage({ params }: { params: Promise<{ lang: string }> }) {
+	const { lang } = await params;
+  console.log(lang)
+
+  const homePage = await client.fetch<SanityDocument>(
+    `*[_type == "page" && language == $lang && pageType == $pageType][0]{ 
+      seo, content[]{
+        "imageUrl": image.asset->url,
+        ...
+      }
+    }`,
+    { lang, pageType: "home" }
+  )
+  console.log(homePage)
+
 	return (
 		<main>
 			<Navbar />
-			<Hero />
+			{homePage.content.map((section: any) => {
+        switch (section._type) {
+          case "heroSection":
+            return <Hero key={section._key} {...section} />
+          default:
+            return null;
+        }
+      })}
 		</main>
 	);
 }
--- client/components/hero.tsx
+++ client/components/hero.tsx
@@ -1,31 +1,62 @@
-import Image from "next/image";
+import { Image } from "next-sanity/image";
 import React from "react";
 
 //todo: JSON-LD
 
-const Hero = () => {
+interface Props {
+	title: string;
+	subtitle: string;
+	imageUrl: string;
+	description: string;
+	ctaBtns: {
+		buttons: {
+			_key: string;
+			style: "primary" | "secondary" | "outline";
+			label: string;
+			internalLink?: {
+				_ref: string;
+				_type: "reference";
+			};
+			externalLink?: string;
+		}[];
+	};
+}
+
+const Hero = ({ title, subtitle, imageUrl, description, ctaBtns }: Props) => {
 	return (
 		<section
 			id="hero"
 			className="w-full h-auto overflow-hidden rounded-b-[4em] py-40 relative"
-		> 
+		>
 			<div className="container relative z-10 text-white flex flex-col items-center justify-center px-4">
-				<h1 className="font-serif text-5xl font-bold text-balance text-center">Affordable, Values-Driven Education in Peerzadiguda</h1>
+				<h1 className="font-serif text-5xl font-bold text-balance text-center">
+					{title}
+				</h1>
 				<h2 className="text-2xl text-center text-balance leading-tight mt-3 mb-6 opacity-80">
-					Empowering Future-Ready Students with Strong Academics & Life Skills
+					{subtitle}
 				</h2>
 				<div className="flex flex-col md:flex-row gap-4">
-					<button className="bg-primary text-white px-6 py-3 rounded-full font-medium">Apply Now</button>
-					<button className="bg-secondary text-black px-6 py-3 rounded-full font-medium">Book a School Tour</button>
+					{ctaBtns.buttons.map((btn) => {
+						return (
+							<button
+								key={btn._key}
+								className={`${btn.style === "primary" ? "bg-primary text-white" : btn.style === "secondary" ? "bg-secondary text-black" : "bg-transparent text-white border border-white"} px-6 py-3 rounded-full font-medium`}
+							>
+								{btn.label}
+							</button>
+						);
+					})}
 				</div>
-				<p className="text-lg text-center mt-4">
-					Krishnaveni School in Peerzadiguda offers affordable, English-medium
-					education rooted in local values and life skills
-				</p>
+				<p className="text-lg text-center mt-4">{description}</p>
 			</div>
 
-      <div className="absolute bg-black opacity-70 w-full h-full z-[2] top-0 left-0"></div>
-      <Image src="/hero-bg.jpg" alt="Hero Background" fill className="object-cover absolute w-full h-full z-[1]" />
+			<div className="absolute bg-black opacity-70 w-full h-full z-[2] top-0 left-0"></div>
+			<Image
+				src={imageUrl}
+				alt="Hero Background"
+				fill
+				className="object-cover absolute w-full h-full z-[1]"
+			/>
 		</section>
 	);
 };
