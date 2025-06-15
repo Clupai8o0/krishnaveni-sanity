0fc39f2628a145d54764016f823165b8285d4ac8
Clupai8o0 <clupaio4@gmail.com>
2025-06-15 12:58:14 +1000

    bringing it slightly higher

--- client/components/feature-cards.tsx
+++ client/components/feature-cards.tsx
@@ -27,7 +27,7 @@ const FeatureCard = ({ title, description, icon, color }: CardProps) => {
 function FeatureCards({ cards }: Props) {
 	return (
 		<div className="max-w-7xl mx-auto px-4 relative z-[12]">
-			<div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 relative -top-8 shadow-md rounded-lg overflow-hidden">
+			<div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 relative -top-10 shadow-md rounded-lg overflow-hidden">
 				{cards.map((card) => (
 					<FeatureCard key={card._key} {...card} />
 				))}
